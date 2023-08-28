f7bf48257e87f14ff401b976c1e85966c77a56cb
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-08-28 17:04:05 +0530

    [sanjeeva] : employess status checked

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalculateEmpWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int is_present = 1;
+            Random random=new Random();
+            int emp_check=random.Next(0,2);
+
+            
+
+
+            if (emp_check==is_present)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            
+
+        }
+    }
+}

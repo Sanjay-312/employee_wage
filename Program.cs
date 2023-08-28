e58f2b29188401ff442303b605820653ad1f358b
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-08-28 19:01:39 +0530

    [sanjay] : wage calculated using switch case

--- Program.cs
+++ Program.cs
@@ -1,37 +1,41 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
 
 namespace CalculateEmpWages
 {
     internal class Program
     {
+        public const int is_full_time = 1;
+
+        public const int is_part_time = 2;
+
+        public const int wage_per_hour = 20;
         static void Main(string[] args)
         {
-            int is_full_time = 1;
+            int total_wage = 0;
 
-            int is_part_time = 2;
+            int total_hours_per_day = 0;
 
             Random random=new Random();
 
             int emp_check=random.Next(0,3);
 
-            int wage_per_hour = 20;
+            switch (emp_check)
+            {
+                case is_full_time:
+                    total_hours_per_day = 8;
+                    break;
+                case is_part_time:
+                    total_hours_per_day = 4;
+                    break;
+                default:
+                    total_hours_per_day=0;
+                    break;
+            
+            }
 
-            int total_wage = 0;
-            int total_hours_per_day = 0;
-
-            if (emp_check==is_full_time)
-            {
-                total_hours_per_day = 8;
-            }
-            else if (emp_check==is_part_time)
-            {
-                total_hours_per_day = 4;
-            }
-            else
-            {
-                total_hours_per_day = 0;
-            }
-            total_wage = wage_per_hour * total_hours_per_day;
+           total_wage = wage_per_hour * total_hours_per_day;
 
             Console.WriteLine("employee wage : "+total_wage);
 

73db0c555816e86052fe9d54b33aaab3598c0191
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-08-28 17:23:58 +0530

    [sanjeeva] : calculated the wage

--- Program.cs
+++ Program.cs
@@ -7,21 +7,28 @@ namespace CalculateEmpWages
         static void Main(string[] args)
         {
             int is_present = 1;
+
             Random random=new Random();
+
             int emp_check=random.Next(0,2);
 
-            
+            int wage_per_hour = 20;
 
+            int total_wage = 0;
+            int total_hours_per_day = 0;
 
             if (emp_check==is_present)
             {
-                Console.WriteLine("Employee is present");
+                total_hours_per_day = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                total_hours_per_day = 0;
             }
-            
+            total_wage = wage_per_hour * total_hours_per_day;
+
+            Console.WriteLine("employee salary per day is : "+total_wage);
+
 
         }
     }

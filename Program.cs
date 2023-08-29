4f12d42bccdbe9497645f050531ed604885ae47a
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-08-29 20:02:40 +0530

    [sanjeeva] : calculated emp wage for each company

--- Program.cs
+++ Program.cs
@@ -6,14 +6,51 @@ namespace CalculateEmpWages
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            
+            EmpWageBulderObject company_1 = new EmpWageBulderObject("D-mart",20,20,100);
+            
+            EmpWageBulderObject company_2=new EmpWageBulderObject("Jio",10,10,50);
+
+            company_1.calculateWages();
+
+            Console.WriteLine("---------------------------------------");
+
+            Console.WriteLine(company_1.toString());
+            Console.WriteLine("----------------------------------------");
+
+            company_2.calculateWages();
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine(company_2.toString());
+            Console.WriteLine("----------------------------------------");
+
+
+        }
+    }
+
+    public class EmpWageBulderObject
+    {
         public const int is_full_time = 1;
 
         public const int is_part_time = 2;
 
-        public static int calculateWages(string company,int wage_per_hour,int no_of_working_days,int max_hrs_in_month)
-        {
-            int total_wage = 0;
+        private string company;
+        private int wage_per_hour;
+        private int no_of_working_days;
+        private int max_hrs_in_month;
+        private int total_wage = 0;
 
+        public EmpWageBulderObject(string compnay,int wage_per_hour,int no_of_working_days,int max_hrs_in_month)
+        {
+            this.company = compnay;
+            this.wage_per_hour = wage_per_hour;
+            this.no_of_working_days = no_of_working_days;
+            this.max_hrs_in_month = max_hrs_in_month;
+
+        }
+        public void calculateWages()
+        {
             int total_hours_per_day = 0;
 
             int total_working_days = 0;
@@ -46,20 +83,19 @@ namespace CalculateEmpWages
                 Console.WriteLine("day " + total_working_days + " emp hrs : " + emp_hours);
 
             }
-            Console.WriteLine("---------------------------------");
 
             total_wage = emp_hours * wage_per_hour;
 
             Console.WriteLine("total emp wage : " + total_wage);
-            return total_wage;
-        }
-        static void Main(string[] args)
-        {
             
-            calculateWages("D-mart",20,20,100);
-            Console.WriteLine("---------------------------------");
-            calculateWages("Jio", 10, 20, 50);
 
+           
+            
+
+        }
+        public string toString()
+        {
+            return "total emp wage for company " + this.company + " is " + this.total_wage;
         }
     }
 }

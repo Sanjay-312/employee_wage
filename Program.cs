443e2903f13ccf94598c2b245fefee038158f977
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-08-29 12:19:10 +0530

    [sanjeeva] : calculated the wage for max of 100 hrs or max days as 20

--- Program.cs
+++ Program.cs
@@ -13,16 +13,22 @@ namespace CalculateEmpWages
         public const int wage_per_hour = 20;
 
         public const int no_of_days_in_month = 20;
+
+        public const int max_hrs_in_month = 100;
         static void Main(string[] args)
         {
             int total_wage = 0;
 
-            int total_wage_per_day = 0;
-
             int total_hours_per_day = 0;
 
-            for (int day=1;day<=no_of_days_in_month;day++)
+            int total_working_days = 0;
+
+            int emp_hours = 0;
+
+            while (emp_hours<=max_hrs_in_month && total_working_days<no_of_days_in_month )
             {
+                total_working_days++;
+
                 Random random = new Random();
 
                 int emp_check = random.Next(0, 3);
@@ -36,20 +42,20 @@ namespace CalculateEmpWages
                         total_hours_per_day = 4;
                         break;
                     default:
-                        total_hours_per_day = 0;
-                        break;
+                       total_hours_per_day = 0;
+                       break;
 
                 }
+                emp_hours += total_hours_per_day;
 
-                total_wage_per_day = wage_per_hour * total_hours_per_day;
-
-                total_wage += total_wage_per_day;
-
-                Console.WriteLine("total wage per day " + day + " is : " + total_wage_per_day);
+                Console.WriteLine("day " + total_working_days + " emp hrs : " +emp_hours);
             
             }
+            Console.WriteLine("---------------------------------------");
 
-            Console.WriteLine("employee wage for 20 days : " + total_wage);
+            total_wage = emp_hours * wage_per_hour;
+
+            Console.WriteLine("total emp wage : " +total_wage);
 
         }
     }
